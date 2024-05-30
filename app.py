6a32d711242c41bf3d7f6c3ceb89f177b81b8a25
apekshaPython <apeksha.unhale10@gmail.com>
2024-05-30 15:12:20 +0530

    updated flask

--- app.py
+++ app.py
@@ -66,4 +66,4 @@ def predict():
         return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0', port = 5005, debug=True)
+    app.run(host = '0.0.0.0', port = 5005, debug=False)
